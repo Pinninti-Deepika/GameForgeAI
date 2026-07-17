3d79bb1ac4d73507ca71d658856485120ad1aa30
Pinninti-Deepika <160014086+Pinninti-Deepika@users.noreply.github.com>
2026-07-17 16:08:28 +0530

    Update CORS middleware to allow multiple origins

--- backend/main.py
+++ backend/main.py
@@ -3,24 +3,26 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from routers.game_router import router as game_router
 
-
 app = FastAPI(
     title="GameForge AI",
     version="1.0.0"
 )
 
+# Allowed frontend URLs
+origins = [
+    "http://localhost:5173",
+    "https://game-forge-ai-sepia.vercel.app",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
 app.include_router(game_router)
-
 
 @app.get("/")
 def home():
